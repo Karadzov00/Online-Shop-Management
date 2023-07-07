694a289b1bd85d6237ce2a3e983225f689acd4a9
Karadzov00 <caka.azdaha@gmail.com>
2023-07-07 03:10:05 +0200

    added some methods in shop operations

--- src/rs/etf/sab/student/ka190444_ShopOperations.java
+++ src/rs/etf/sab/student/ka190444_ShopOperations.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -82,6 +83,7 @@ public class ka190444_ShopOperations implements ShopOperations {
                         ps.setInt(1, idCity);
                         ps.setInt(2, i);
                         ps.executeUpdate();
+                        System.out.println("shop updated, new id city set:"+idCity);
                         return 1; 
                     } catch (SQLException ex) {
                         Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
@@ -98,27 +100,121 @@ public class ka190444_ShopOperations implements ShopOperations {
 
     @Override
     public int getCity(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Connection conn = DB.getInstance().getConnection();
+        String query = "select idCity from Shops where IdShop=?";
+        try (
+            PreparedStatement stmt = conn.prepareStatement(query);  ) {
+            stmt.setInt(1, i);
+            try(ResultSet rs = stmt.executeQuery()) {
+                if(rs.next()) {
+                    System.out.println("the id of the city is:"+rs.getInt(1));
+                    return rs.getInt(1); 
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1; 
     }
 
     @Override
     public int setDiscount(int i, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Connection conn = DB.getInstance().getConnection();
+        String query ="update Shops set Discount=? where IdShop=?"; 
+        try ( PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setInt(1, i1);
+            ps.setInt(2, i);
+            if(ps.executeUpdate()==1){
+                System.out.println("shop updated, discount set to :"+i1);
+                return 1;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1; 
     }
 
     @Override
     public int increaseArticleCount(int i, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Connection conn = DB.getInstance().getConnection();
+        String query ="update Articles set Amount=Amount+? where IdArticle=?";
+        try ( PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setInt(1, i1);
+            ps.setInt(2, i);//article id
+            ps.executeUpdate(); 
+            String querySelect = "select Amount from Articles where IdArticle=?";
+            try (
+                PreparedStatement stmt = conn.prepareStatement(query)) {
+                stmt.setInt(1, i);
+                try(ResultSet rs = stmt.executeQuery()) {
+                    if (rs.next()) {
+                        System.out.println("new amount of selected article:"+rs.getInt(1));
+                        return rs.getInt(1); 
+                    }
+                } catch (SQLException ex) {
+                Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1; 
     }
 
     @Override
     public int getArticleCount(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Connection conn = DB.getInstance().getConnection();
+        String query = "select COUNT(*)\n" +
+                        "from Articles\n" +
+                        "where IdShop=?\n" +
+                        "group by IdShop"; 
+        try (
+            PreparedStatement stmt = conn.prepareStatement(query);  ) {
+            stmt.setInt(1, i);
+            try(ResultSet rs = stmt.executeQuery()) {
+                if(rs.next()) {
+                    System.out.println("number of articles in given shop is:"+rs.getInt(1));
+                    return rs.getInt(1); 
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0; 
+
     }
 
     @Override
     public List<Integer> getArticles(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Connection conn = DB.getInstance().getConnection();
+        String query = "select IdArticle\n" +
+                        "from Articles\n" +
+                        "where IdShop=?"; 
+        List<Integer> articles = new ArrayList<>(); 
+        try (
+            PreparedStatement stmt = conn.prepareStatement(query);  ) {
+            stmt.setInt(1, i);
+            try(ResultSet rs = stmt.executeQuery()) {
+                System.out.println("articles");
+                while(rs.next()) {
+                    System.out.println(rs.getInt(1));
+                    articles.add(rs.getInt(1)); 
+                    
+                }
+                return articles; 
+            } catch (SQLException ex) {
+                Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ka190444_ShopOperations.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null; 
     }
 
     @Override
