293856d4f2225f9d4043f9cbedb8909078b6bf81
Karadzov00 <caka.azdaha@gmail.com>
2023-07-08 21:41:48 +0200

    updated dijkstra's algorithm output format

--- src/rs/etf/sab/student/DijkstraAlgorithm.java
+++ src/rs/etf/sab/student/DijkstraAlgorithm.java
@@ -14,11 +14,9 @@ import java.util.*;
 public class DijkstraAlgorithm {
     private static final int INFINITY = Integer.MAX_VALUE;
 
-
     public static void dijkstra(Map<Integer, Map<Integer, Integer>> graph, int startVertex) {
         Map<Integer, Integer> distances = new HashMap<>();
         Map<Integer, Integer> previousVertices = new HashMap<>();
-        Set<Integer> visited = new HashSet<>();
         PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>();
 
         for (int vertex : graph.keySet()) {
@@ -33,12 +31,6 @@ public class DijkstraAlgorithm {
             Vertex currentVertex = priorityQueue.poll();
             int currentCity = currentVertex.city;
 
-            if (visited.contains(currentCity)) {
-                continue;
-            }
-
-            visited.add(currentCity);
-
             for (Map.Entry<Integer, Integer> neighbor : graph.get(currentCity).entrySet()) {
                 int neighborCity = neighbor.getKey();
                 int distance = neighbor.getValue();
@@ -51,7 +43,25 @@ public class DijkstraAlgorithm {
             }
         }
 
+        printGraph(graph);
         printShortestPaths(startVertex, distances, previousVertices);
+    }
+
+    private static void printGraph(Map<Integer, Map<Integer, Integer>> graph) {
+        System.out.println("Graph:");
+        for (Map.Entry<Integer, Map<Integer, Integer>> entry : graph.entrySet()) {
+            int sourceVertex = entry.getKey();
+            Map<Integer, Integer> neighbors = entry.getValue();
+
+            System.out.print(sourceVertex + ": ");
+            for (Map.Entry<Integer, Integer> neighbor : neighbors.entrySet()) {
+                int destinationVertex = neighbor.getKey();
+                int distance = neighbor.getValue();
+                System.out.print(destinationVertex + "(" + distance + ") ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     private static void printShortestPaths(int startVertex, Map<Integer, Integer> distances,
@@ -62,7 +72,18 @@ public class DijkstraAlgorithm {
             int distance = entry.getValue();
 
             List<Integer> path = buildPath(destination, previousVertices);
-            System.out.println("Destination: " + destination + "\tDistance: " + distance + "\tPath: " + path);
+            System.out.print("Destination: " + destination + "\tDistance: " + distance + "\tPath: ");
+            for (int i = 0; i < path.size(); i++) {
+                int vertex = path.get(i);
+                if (i == 0) {
+                    System.out.print(vertex);
+                } else {
+                    int previousVertex = path.get(i - 1);
+                    int vertexDistance = distances.get(vertex) - distances.get(previousVertex);
+                    System.out.print(" (" + vertex + " (" + vertexDistance + "))");
+                }
+            }
+            System.out.println();
         }
     }
 
@@ -81,24 +102,8 @@ public class DijkstraAlgorithm {
 
         return path;
     }
-    
-     private static void printGraph(Map<Integer, Map<Integer, Integer>> graph) {
-        System.out.println("Graph:");
-        for (Map.Entry<Integer, Map<Integer, Integer>> entry : graph.entrySet()) {
-            int sourceVertex = entry.getKey();
-            Map<Integer, Integer> neighbors = entry.getValue();
 
-            System.out.print(sourceVertex + ": ");
-            for (Map.Entry<Integer, Integer> neighbor : neighbors.entrySet()) {
-                int destinationVertex = neighbor.getKey();
-                int distance = neighbor.getValue();
-                System.out.print(destinationVertex + "(" + distance + ") ");
-            }
-            System.out.println();
-        }
-    }
-    
-    public static void formGraph(){
+    public static Map<Integer, Map<Integer, Integer>> formGraph(){
         Connection conn = DB.getInstance().getConnection();
         Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
 
@@ -116,17 +121,22 @@ public class DijkstraAlgorithm {
             }
 
             printGraph(graph);
-            dijkstra(graph, 232);
-
+            
             resultSet.close();
             statement.close();
+
+            return graph; 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null; 
     }
 
     public static void main(String[] args) {
-        formGraph();
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+        graph = formGraph();
+        dijkstra(graph, 232);
+
     }
 
     static class Vertex implements Comparable<Vertex> {
@@ -144,4 +154,3 @@ public class DijkstraAlgorithm {
         }
     }
 }
-
